c116b357bb4716688dd32e05ad93e262a9de1e4e
benserg <bendrysevsergej@gmail.com>
2023-12-05 20:20:07 +0300

    graph-interface update

--- main_window.py
+++ main_window.py
@@ -3,7 +3,13 @@ from PyQt5.QtWidgets import QWidget, QStackedWidget, QMainWindow, QVBoxLayout, Q
 
 
 class MainWindow(QWidget):
+    def goto_article_window(self):
+        self.article_window = ArticleWindow()
+        self.article_window.show()
+        self.close()
+
     def __init__(self):
+        self.article_window = None
         self.amount_button = None
         self.journal_button = None
         self.budget_change_button = None
@@ -35,78 +41,66 @@ class MainWindow(QWidget):
         self.setGeometry(0, 0, 800, 600)
         font = self.create_font()
 
-        self.operations_button = self.create_button(0, 0, 400, 200, font, 'Менеджер операций')
-        self.article_button = self.create_button(400, 0, 400, 200, font, 'Менеджер квитанций')
+        self.operations_button = self.create_button(0, 0, 400, 200, font=font, object_name='Менеджер операций')
+        self.article_button = self.create_button(400, 0, 400, 200, font=font, object_name='Менеджер квитанций')
         self.streams_button = self.create_button(400, 200, 400, 200, font, 'Динамика потоков по квитанциям',
                                                  background_color='blue')
         self.budget_change_button = self.create_button(0, 200, 400, 200, font, 'Динамика изменения бюджета',
                                                        background_color='blue')
         self.journal_button = self.create_button(0, 400, 400, 200, font, 'Журнал анализа', background_color='red')
         self.amount_button = self.create_button(400, 400, 400, 200, font, 'Отображение прибыли', background_color='red')
-
+        self.article_button.clicked.connect(self.goto_article_window)
         self.setLayout(self.layout)
 
 
-class ArticleWindow(QMainWindow):
+class ArticleWindow(QWidget):
+    def return_to_main_menu(self):
+        self.main_menu = MainWindow()
+        self.main_menu.show()
+        self.close()
 
     def __init__(self):
+        self.main_menu = None
+        self.enter_button = None
+        self.return_button = None
+        self.modify_button = None
+        self.layout = QVBoxLayout()
         super().__init__()
-        self.setupUi(QtWidgets.QMainWindow())
+        self.setupUi()
 
-    def setupUi(self, mainWindow):
-        mainWindow.setObjectName("mainWindow")
-        mainWindow.resize(800, 600)
-        self.centralwidget = QtWidgets.QWidget(mainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.return_button = QtWidgets.QPushButton(self.centralwidget)
-        self.return_button.setGeometry(QtCore.QRect(0, 0, 91, 51))
+    def setupUi(self):
+        self.setWindowTitle('Менеджер квитанций')
+        self.setGeometry(0, 0, 800, 600)
         font = QtGui.QFont()
         font.setFamily("DejaVu Serif")
         font.setPointSize(41)
         font.setBold(True)
         font.setWeight(75)
+
+        self.return_button = QPushButton('←', self)
+        self.return_button.setGeometry(QtCore.QRect(0, 0, 91, 51))
         self.return_button.setFont(font)
         self.return_button.setObjectName("return_button")
-        self.return_button.clicked.connect(self.on_click)
-        self.enter_button = QtWidgets.QPushButton(self.centralwidget)
+        self.return_button.clicked.connect(self.return_to_main_menu)
+
+        self.enter_button = QtWidgets.QPushButton('Регистрация квитанция', self)
         self.enter_button.setGeometry(QtCore.QRect(174, 50, 471, 125))
-        font = QtGui.QFont()
         font.setFamily("Serif")
         font.setPointSize(20)
         font.setBold(True)
         font.setWeight(75)
         self.enter_button.setFont(font)
         self.enter_button.setObjectName("enter_button")
-        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_3.setGeometry(QtCore.QRect(180, 220, 471, 125))
-        font = QtGui.QFont()
-        font.setFamily("Serif")
-        font.setPointSize(20)
-        font.setBold(True)
-        font.setWeight(75)
-        self.pushButton_3.setFont(font)
-        self.pushButton_3.setObjectName("pushButton_3")
-        self.pushButton_4 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_4.setGeometry(QtCore.QRect(175, 400, 471, 125))
-        font = QtGui.QFont()
-        font.setFamily("Serif")
-        font.setPointSize(20)
-        font.setBold(True)
-        font.setWeight(75)
-        self.pushButton_4.setFont(font)
-        self.pushButton_4.setObjectName("pushButton_4")
-        mainWindow.setCentralWidget(self.centralwidget)
 
-        self.retranslateUi(mainWindow)
-        QtCore.QMetaObject.connectSlotsByName(mainWindow)
+        self.modify_button = QtWidgets.QPushButton('Модификация квитанций', self)
+        self.modify_button.setGeometry(QtCore.QRect(180, 220, 471, 125))
+        self.modify_button.setFont(font)
 
-    def retranslateUi(self, mainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        mainWindow.setWindowTitle(_translate("mainWindow", "Менеджер статей"))
-        self.return_button.setText(_translate("mainWindow", "←"))
-        self.enter_button.setText(_translate("mainWindow", "Ввод статей"))
-        self.pushButton_3.setText(_translate("mainWindow", "Модификация статей"))
-        self.pushButton_4.setText(_translate("mainWindow", "Удаление статей"))
+        self.delete_button = QtWidgets.QPushButton('Удаление квитанций', self)
+        self.delete_button.setGeometry(QtCore.QRect(175, 400, 471, 125))
+        self.delete_button.setFont(font)
+
+        self.setLayout(self.layout)
 
 
 if __name__ == "__main__":
